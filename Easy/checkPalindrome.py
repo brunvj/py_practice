95d84d0ca60672fc0801a9765f4f648b17ba7a63
Vincente Bruno <vincentejbruno@outlook.com>
2022-09-29 19:13:01 -0400

    Create checkPalindrome.py

---
+++ Easy/checkPalindrome.py
@@ -0,0 +1,16 @@
+# CodeSignal Assigment:
+# Given a string with constraints of 4 seconds runtime max, string inputString, 1<= inputString.length <= 10^5
+# output if the string is a palindrome or not
+
+# My code:
+def solution(inputString):
+    # compare ends of the string to each other and move in towards the center of the string
+    # 
+    # x = len(inputString)
+    # y = x / 2
+    # z = y - 0.5
+    # for char in iS:
+    #   if iS[char] == iS[x]:
+    #     
+    #   else:
+    #       return False
