a054ee12f7dba9c1d5a934221b52ae05f71104d3
Vincente Bruno <vincentejbruno@outlook.com>
2022-10-07 20:25:58 -0400

    Update old_macdonald.py

--- Easy/old_macdonald.py
+++ Easy/old_macdonald.py
@@ -9,3 +9,11 @@ def old_macdonald(name):
     newname[0] = newname[0].upper()
     newname[3] = newname[3].upper()
     return ''.join(newname)
+
+# More efficient solution:
+
+def old_macdonald(name):
+     first_half = name[:3]
+     second_half = name[3:]
+     
+     return first_half.capitalize() + second_half.capitalize()
