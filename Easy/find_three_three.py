29c04360f6b3d686270f272054ddc93e17af0de9
Vincente Bruno <vincentejbruno@outlook.com>
2022-10-07 21:26:49 -0400

    Update find_three_three.py

--- Easy/find_three_three.py
+++ Easy/find_three_three.py
@@ -8,19 +8,15 @@ Given a list of ints, return True if the array contains a 3 next to a 3 somewher
     
     
 def has_33(nums):
-    # initialize range value
-    x = len(nums)
     # for n in the range of the length of the list
-    for n in range(x):
-        print(n)
-        # if nums[n] == 3
-        if nums[n] == 3:
-            # if nums[n] == nums[n+1] or nums[n] == nums[n-1], return True
-            if nums[n] == nums[n+1] or nums[n] == nums[n+1]:
-                return True
-            # else, return false
-            else:
-                return False
-        # else, continue
-        else:       
-            continue
+    for n in range(len(nums)):
+        if nums[n] == 3 and nums[n+1] == 3:
+            return True
+    return False
+
+# More efficient, less readable:
+def has_33(nums):
+    for i in range(0, len(nums) - 1):
+        if nums[i:i+2] == [3,3]:
+            return True
+    return False
