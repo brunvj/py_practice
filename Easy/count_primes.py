121d8885a7d2161ce0310fedeb369d5d6b7929f1
Vincente Bruno <vincentejbruno@outlook.com>
2022-10-08 10:46:55 -0400

    Updated practice problem from Udemy

--- Easy/count_primes.py
+++ Easy/count_primes.py
@@ -20,3 +20,29 @@ def count_primes(num):
                 count += 1
     # Finally, print the total count of numbers leading up to the input number
     print(count)
+    
+    
+# More efficient:
+
+def count_primes(num):
+    # Check for 0 or 1 input
+    if num < 2:
+        return 0
+    # Check for 2 or greater
+    
+    # Store our prime numbers
+    primes = [2]
+    # Counter going up to the input num
+    x = 3
+    # x is going through every number up to input num
+    while x <= num:
+        # Check if x is prime
+        for y in primes:
+            if x%y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+    print(primes)
+    return len(primes)
