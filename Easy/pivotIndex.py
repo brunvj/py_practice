4b234b44f35bbedb34e56b25f37b84b72fd389a0
Vincente Bruno <vincentejbruno@outlook.com>
2022-10-17 11:32:45 -0400

    Create pivotIndex.py

---
+++ Easy/pivotIndex.py
@@ -0,0 +1,17 @@
+# pivot index = sum of all numbers to the left of the index
+        x = int(len(nums)/2)
+        # left side and right side initialization
+        lp = []
+        rp = []
+        # left side calc: calculate sum at range 0 to len/2
+        for i in range(0, x):
+            lp[0] += nums[x]
+        # right side calc: calculate sum at range len/2 to len
+
+        # return the left most pivot index
+        # if the pivot 
+        if():
+            pass
+        # if no index exists
+        else:
+            return -1
